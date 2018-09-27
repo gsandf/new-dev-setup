726cfc12ee61290f7bf3b3655a73029ca2e36339
Blake Knight <bknight@gsandf.com>
2018-09-27 09:03:36 -0500

    :sparkles: install `brew cask` & Yarn, fix n folder permissions

--- src/execute.js
+++ src/execute.js
@@ -14,4 +14,17 @@ function execute(file, args, options) {
   return command;
 }
 
-export default execute;
+/**
+ * Executes a command and handles piping stderr/stdout
+ * @param {String} script - the script to run in a shell
+ * @param {Object} options - options passed to `execa`
+ * @returns {Promise} Resolves when command has completed running
+ */
+function executeShell(script, options) {
+  const command = execa.shell(script, options);
+  command.stdout.pipe(process.stdout);
+  command.stderr.pipe(process.stderr);
+  return command;
+}
+
+export { execute, executeShell };
